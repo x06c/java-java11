54f8ecd104fcf8f73a5a33fad4eb413c7784733a
x06c <jonathan.miss@outlook.com>
2021-05-28 14:45:50 +0200

    commit ex6

--- src/test/java/java8/ex06/DateAndTime_06_Test.java
+++ src/test/java/java8/ex06/DateAndTime_06_Test.java
@@ -3,6 +3,7 @@ package java8.ex06;
 import org.junit.Test;
 
 import java.time.LocalDate;
+import java.time.Month;
 import java.time.Period;
 
 import static org.hamcrest.CoreMatchers.*;
@@ -17,13 +18,13 @@ public class DateAndTime_06_Test {
     public void test_period() throws Exception {
 
         // TODO Créer une LocalDate au 31/12/2017
-        LocalDate localDate1 = null;
+        LocalDate localDate1 = LocalDate.of(2017,Month.DECEMBER,31);
 
         // TODO Créer une LocalDate au 01/01/2050
-        LocalDate localDate2 = null;
+        LocalDate localDate2 = LocalDate.of(2050, Month.JANUARY,1);
 
         // TODO créer une période (classe java.time.Period) à entre les 2 précédentes dates
-        Period result = null;
+        Period result = Period.between(localDate1, localDate2);
 
         assertThat(result.toTotalMonths(), is(384L));
     }
