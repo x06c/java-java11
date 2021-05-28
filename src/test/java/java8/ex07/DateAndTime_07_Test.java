cf47443a6883319c1d36badaebe7aea6a96fb7b1
x06c <jonathan.miss@outlook.com>
2021-05-28 14:57:22 +0200

    commit ex7

--- src/test/java/java8/ex07/DateAndTime_07_Test.java
+++ src/test/java/java8/ex07/DateAndTime_07_Test.java
@@ -2,9 +2,8 @@ package java8.ex07;
 
 import org.junit.Test;
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.Month;
+import java.time.*;
+import java.util.Calendar;
 import java.util.Date;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -19,13 +18,24 @@ public class DateAndTime_07_Test {
     public void test_date_to_localdate() throws Exception {
 
         // TODO Créer une date Java 1 (12/02/2017)
-        Date date = null;
+//        Version avec Calendar
+//        Calendar cal = Calendar.getInstance();
+//        cal.set(2017,1,12);
+//        Date date = cal.getTime();
+
+        Date date = new Date(2017-1900, Calendar.FEBRUARY,12);
+
 
         // TODO transformer la date en Instant
+        Instant instant = date.toInstant();
 
-        // TODO transformer la date en LocalDate
 
-        LocalDate result = null;
+        // TODO transformer la date en LocalDate, utilisation ZoneDateTime.class pour conversion / method toLocalDate() conversion ZoneDateTime -> LocalDate
+
+        ZonedDateTime zonedDateTime = ZonedDateTime.ofInstant(instant, ZoneId.systemDefault());
+
+        LocalDate result = zonedDateTime.toLocalDate();
+
 
         // TODO valoriser les différentes variables afin de rendre le test passant
 
