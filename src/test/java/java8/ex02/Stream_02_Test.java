547b48955843c112a5d8bf31c86be99d401be1e2
x06c <jonathan.miss@outlook.com>
2021-05-31 15:58:18 +0200

    commit ex2

--- src/test/java/java8/ex02/Stream_02_Test.java
+++ src/test/java/java8/ex02/Stream_02_Test.java
@@ -24,7 +24,8 @@ public class Stream_02_Test {
         List<Order> orders = new Data().getOrders();
 
         // Trouver la liste des clients ayant déjà passés une commande
-        List<Customer> result = null;
+
+        List<Customer> result = orders.stream().filter(order -> !order.getPizzas().isEmpty()).map(order -> order.getCustomer()).distinct().collect(Collectors.toList());
 
         assertThat(result, hasSize(2));
     }
