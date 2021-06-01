260e10356361d6853612976f9155d33e0e93abab
x06c <jonathan.miss@outlook.com>
2021-06-01 14:59:20 +0200

    commit ex5 .1 .2

--- src/test/java/java8/ex05/Stream_05_Test.java
+++ src/test/java/java8/ex05/Stream_05_Test.java
@@ -34,7 +34,8 @@ public class Stream_05_Test {
     // TODO utiliser la méthode Stream.iterate
     // TODO cette méthode doit produire le même résultat que imperativeSum
     private long iterateSum(long n) {
-        return 0;
+        return Stream.iterate(0L, v -> v + 1L).limit(n).reduce(0L, Long::sum);
+
     }
 
     // TODO exécuter le test pour vérifier que les méthodes imperativeSum et iterateSum produisent le même résultat
@@ -53,7 +54,7 @@ public class Stream_05_Test {
     // TODO utiliser la méthode Stream.iterate
     // TODO transformer en stream parallel (.parallel())
     private long parallelIterateSum(long n) {
-        return 0;
+        return Stream.iterate(0L, v -> v + 1L).limit(n).parallel().reduce(0L, Long::sum);
     }
 
     // TODO exécuter le test pour vérifier que les méthodes imperativeSum, iterateSum et parallelIterateSum produisent le même résultat
